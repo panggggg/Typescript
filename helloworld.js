47369eed1c3c22e173c13ac904135a3b0722ea6c
panggggg <p.picczy@gmail.com>
2021-06-07 12:12:27 +0700

    no message

---
+++ helloworld.js
@@ -0,0 +1,11 @@
+// function add(number1: number, number2: number) {
+//     return number1 + number2;
+// }
+// console.log(add(2, 2));
+// console.log(add(2, 7));
+var myArr;
+myArr = ["1", "2", "3", "4"];
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
---
+++ helloworld.ts
@@ -0,0 +1,15 @@
+// function add(number1: number, number2: number) {
+//     return number1 + number2;
+// }
+
+// console.log(add(2, 2));
+// console.log(add(2, 7));
+
+var myArr: string[];
+
+myArr = ["1", "2", "3", "4"];
+
+console.log(myArr[0]);
+console.log(myArr[1]);
+console.log(myArr[2]);
+console.log(myArr[3]);
