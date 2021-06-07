3b47b49055cff37a984d776685f011fc5aab411b
panggggg <p.picczy@gmail.com>
2021-06-07 12:12:11 +0700

    no message

---
+++ datatype.js
@@ -0,0 +1,4 @@
+var num1 = 5;
+var first_name = "Pawornwan";
+var isPresent = true;
+var myVar = "This is my variable";
---
+++ datatype.ts
@@ -0,0 +1,4 @@
+let num1: number = 5;
+let first_name: string = "Pawornwan";
+let isPresent: boolean = true;
+let myVar: any = "This is my variable";
